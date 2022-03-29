19b7702fdce688c4d3413afe608cde0e1c2b44a3
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-30 02:39:50 +0530

    Add project files.

---
+++ Array_sory_by _string_char/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Array_sory_by__string_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] ar = { "Ajay", "Ram", "Aman", "Shyam" };
+            
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if(ar[i][0]=='A')
+                Console.WriteLine(ar[i]);
+
+            }
+
+        }
+    }
+}
